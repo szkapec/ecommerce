df8345b3a345cfb687a548bb377fde97eeec3d66
szkapec <szkapec999@o2.pl>
2020-11-08 15:31:40 +0100

    Updates

---
+++ precache-manifest.b33218d23fb9b8c5ca69f163fbb718dd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b24d312e4f114a451a16ef43919379f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "901b8d46e17c93588fe4",
+    "url": "/static/css/2.437d98bc.chunk.css"
+  },
+  {
+    "revision": "9161a83403a68d377ad0",
+    "url": "/static/css/main.4a4d73e6.chunk.css"
+  },
+  {
+    "revision": "901b8d46e17c93588fe4",
+    "url": "/static/js/2.17067a3b.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.17067a3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9161a83403a68d377ad0",
+    "url": "/static/js/main.4c69dc5b.chunk.js"
+  },
+  {
+    "revision": "3cc9d6562f64fc197f20",
+    "url": "/static/js/runtime-main.fc30dc24.js"
+  },
+  {
+    "revision": "b6c5bdf2a1dc0a2bbf14498e387b6c4c",
+    "url": "/static/media/ball.b6c5bdf2.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ecommerce/precache-manifest.855f9bb32a748e732484050e53c805ca.js"
+  "/precache-manifest.b33218d23fb9b8c5ca69f163fbb718dd.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ecommerce/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
