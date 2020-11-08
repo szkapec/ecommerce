8a02361a2e2e9927851f9c4e19102f285d43349e
szkapec <szkapec999@o2.pl>
2020-11-08 15:34:18 +0100

    Updates

---
+++ precache-manifest.0b244f9181632607b91bf97c21b2abba.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f044226026567f529c723f5982bf27c",
+    "url": "/ecommerce/index.html"
+  },
+  {
+    "revision": "1e9b3550f8f1636215cf",
+    "url": "/ecommerce/static/css/2.437d98bc.chunk.css"
+  },
+  {
+    "revision": "9161a83403a68d377ad0",
+    "url": "/ecommerce/static/css/main.4a4d73e6.chunk.css"
+  },
+  {
+    "revision": "1e9b3550f8f1636215cf",
+    "url": "/ecommerce/static/js/2.10a4544c.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/ecommerce/static/js/2.10a4544c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9161a83403a68d377ad0",
+    "url": "/ecommerce/static/js/main.4c69dc5b.chunk.js"
+  },
+  {
+    "revision": "831a87ab3375c7c427c2",
+    "url": "/ecommerce/static/js/runtime-main.ee40b3dc.js"
+  },
+  {
+    "revision": "b6c5bdf2a1dc0a2bbf14498e387b6c4c",
+    "url": "/ecommerce/static/media/ball.b6c5bdf2.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ecommerce/static/media/logo.5d5d9eef.svg"
+  }
+]);
